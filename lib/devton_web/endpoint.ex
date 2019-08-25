7de8eb0e9a9b76e476c6af1f5d6fc44f04ab4a10
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-08-26 01:10:31 +0200

    removed unecessary user_socket

--- lib/devton_web/endpoint.ex
+++ lib/devton_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule DevtonWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :devton
 
-  socket "/socket", DevtonWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
