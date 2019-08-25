892cd37b368d0f6963a0ef24eb887420db01be40
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-08-26 00:57:47 +0200

    update test suite

--- test/devton/workspaces_test.exs
+++ test/devton/workspaces_test.exs
@@ -16,13 +16,14 @@ defmodule Devton.Workspaces.WorkspacesTest do
     assert is_binary(uuid) == true
   end
 
-#  test "opens account with invalid command" do
-#    params = %{
-#      "name" => "test_name"
-#    }
-#
-#    assert {:ok, %Workspace{ uuid: uuid } } = Workspaces.create_workspace(params)
-#    assert is_binary(uuid) == true
-#  end
+  test "opens account with invalid command" do
+    params = %{
+      "name" => "test_name",
+      "token" => nil,
+      "enabled" => false,
+    }
+
+    assert { :error, _, _, _ } = Workspaces.create_workspace(params)
+  end
 
 end
