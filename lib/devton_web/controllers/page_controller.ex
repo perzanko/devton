dd4f716542139555ea7aa81e40edd961458541ea
Kacper Perzankowski <perzankowski.kacper@gmail.com>
2019-09-04 06:21:01 +0200

    add confirmation page

--- lib/devton_web/controllers/page_controller.ex
+++ lib/devton_web/controllers/page_controller.ex
@@ -1,7 +1,22 @@
 defmodule DevtonWeb.PageController do
   use DevtonWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  #  def index(conn, %{}), do: render(conn, "index.html", success: false)
+  def index(conn, params) do
+    success_img = case params["success"] == "true" do
+      true ->
+        result = HTTPoison.get!("https://api.giphy.com/v1/gifs/random?api_key=Xp4xStMRfHflzxprqaqEWw3WJknB1FdZ&tag=congrats&rating=r").body
+                 |> Poison.decode!
+        result["data"]["embed_url"]
+      false -> ""
+    end
+    render(
+      conn,
+      "index.html",
+      %{
+        success: params["success"] == "true",
+        success_img: success_img,
+      }
+    )
   end
 end
